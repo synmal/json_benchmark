051f2b6126ae5f244b5218f23c89d5062a96ffef
Malcolm Jeremiah Rajeli <maljer@fortnynja.com>
2021-11-02 13:37:19 +0800

    Association and seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+25.times do
+  User.create!(username: Faker::Internet.username)
+end
+
+User.all.each do |user|
+  5.times do
+    user.posts.create!(content: Faker::Lorem.paragraph(sentence_count: (5..10).to_a.sample))
+  end
+end
+
+Post.all.each do |post|
+  25.times do
+    post.comments.create!(content: Faker::Lorem.paragraph(sentence_count: (2..3).to_a.sample), user: User.all.sample)
+  end
+end
